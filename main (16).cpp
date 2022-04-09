be7d96cb92bc3c0beb73ded326ce7d475249ff87
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-04-09 20:19:49 +0530

    Add files via upload

---
+++ main (16).cpp
@@ -0,0 +1,34 @@
+// check for pythagorean triplet
+
+#include<iostream>
+using namespace std;
+
+bool check(int x,int y,int z){
+  int a = max(x,max(y,z));
+  int b,c;
+
+  if(a==x){
+    b=y;
+    c=z;
+  }
+  else if(a==y){
+    b=x;
+    c=z;
+  }
+  else{
+    b=x;
+    c =y;
+  }
+  if(a*a == b*b + c*c)
+    return true;
+  return false;
+  
+  
+}
+
+int main(){
+  int x,y,z;
+  cout<<"enter the number :"<<endl;
+  cin>>x>>y>>z;
+  cout<<check(x,y,z);
+}
